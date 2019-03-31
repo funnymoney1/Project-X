b839766a0c418d5527c4a94ac2851cb370294325
funnymoney1 <wvt31244@zwoho.com>
2019-03-31 19:05:55 +0530

    Main links
    New School and College Div

--- wincludes/main.php
+++ wincludes/main.php
@@ -1,19 +1,37 @@
 <div id="main0">
-  <div class="lnav">
-    Welcome ..
-  </div>
   <header class="headbox">
+    <div class="lnav">
+      <div class="wel">
+        Welcome ..
+      </div>
+    </div>
     <span onclick="openNav()" id="sp">&#187;</span>
   </header>
   <div class="firstd">
     <p>You Can Find Some Notes and pdf's here which is related to your course</p>
     <span class="fspn">Please select your stream - </span>
-    <div class="fld container">
+    <div class="sld container-fluid">
+        <div class="slf col-xs-6">
+          <a href="#">
+            <div class="slff">
+              School
+            </div>
+          </a>
+        </div>
+        <div class="sls col-xs-6">
+          <a href="#">
+            <div class="slfd">
+              COLLEGE
+            </div>
+          </a>
+        </div>
+    </div>
+    <!--<div class="fld container">
       <div class="flf col-xs-4"><a href="#" class="fdlink">Bsc</a></div>
       <div class="fls col-xs-4"><a href="pdfs.php" class="fdlink">Bca</a></div>
       <div class="flt col-xs-4"><a href="middle.php" class="fdlink">Bba</a></div>
     </div>
-    <!--<div class="alertd">
+    <div class="alertd">
       <a href="#">cs</a>
       <a href="#">Mt</a>
     </div>-->
